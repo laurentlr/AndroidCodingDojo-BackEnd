8ae7361a8fcd301c40991abc07af568997f873c2
laurent russier <laurent.russier@gmail.com>
2020-03-11 11:08:31 +0100

    Clean action routing

--- src/main/kotlin/application/routing/actions/Routing.kt
+++ src/main/kotlin/application/routing/actions/Routing.kt
@@ -3,6 +3,7 @@ package application.routing.actions
 import application.routing.fcm.Fcm
 import data.DataBase
 import domain.Action
+import domain.Response
 import io.ktor.application.call
 import io.ktor.request.receive
 import io.ktor.response.respond
@@ -16,24 +17,21 @@ fun Routing.actions() {
         call.respond(DataBase.actions)
     }
 
-    post("/actions") {
+    post("/action") {
         var action: Action? = null
-        lateinit var responseMessage: String
+        lateinit var message: String
 
         try {
-            action = call.receive<Action>()
-            responseMessage = "Success"
+            action = call.receive()
+            message = "Success"
         } catch (e: Exception) {
-            println(e)
-            responseMessage = "Error, add header Content-Type:application/json or Action data is wrong !!! $e"
+            message = "Error, add header Content-Type:application/json or Action data is wrong !!! $e"
         }
 
-        call.respond(responseMessage)
-        action?.let {
-            DataBase
-                    .actions
-                    .add(it)
+        call.respond(Response(message))
 
+        action?.let {
+            DataBase.actions.add(it)
             Fcm.onActionAdded(it)
         }
     }
